7e620779c42e94b5ca7f3116162276ab945b83d2
Mishaal Rahman <mishalrm@gmail.com>
2026-09-15 17:16:45 +0400

    Added eclipse and shadow casts on earth and moon

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000,
 );
-camera.position.set(20, 30, 25);
+camera.position.set(10, 20, 25);
 
 // 2. Renderer Configuration
 const canvas = document.querySelector("canvas.threejs");
@@ -21,6 +21,8 @@ const renderer = new THREE.WebGLRenderer({
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 renderer.outputColorSpace = THREE.SRGBColorSpace; // Modern color management
+renderer.shadowMap.enabled = true;
+renderer.shadowMap.type = THREE.PCFShadowMap;
 
 // 3. Controls
 const controls = new OrbitControls(camera, canvas);
@@ -45,10 +47,11 @@ const sunMat = new THREE.MeshStandardMaterial({
   emissiveMap: sunTexture,
   emissive: new THREE.Color(0xffffaa),
   emissiveIntensity: 1.4,
+  roughness: 1,
 });
 const earthMat = new THREE.MeshStandardMaterial({
   map: earthTexture,
-  color: "cyan",
+  color: 0x00aacc,
 });
 
 const moonTexture = textureLoader.load("/textures/2k_moon.jpg");
@@ -66,17 +69,30 @@ earth.scale.setScalar(2);
 earth.position.x = 20;
 
 const moon = new THREE.Mesh(sphereGeo, moonMat);
-moon.scale.setScalar(0.25);
+moon.scale.setScalar(0.1);
 moon.position.x = 2;
 earth.add(moon);
 
 scene.add(sun, earth);
 
+// Sun does not need to cast/receive shadows
+earth.castShadow = true;
+earth.receiveShadow = true;
+
+moon.castShadow = true;
+moon.receiveShadow = true;
+
 // 5. Lighting Setup
 const pointLight = new THREE.PointLight(0xffffee, 8000, 0, 2); // Color, intensity, distance, decay
 // pointLight.position.set(15, 1, 1);
 
-const ambLight = new THREE.AmbientLight(0xffffff, 0);
+pointLight.castShadow = true;
+// Tweak shadow map resolution and bias for crispness and to prevent self-shadowing artifacts
+pointLight.shadow.mapSize.width = 2048;
+pointLight.shadow.mapSize.height = 2048;
+pointLight.shadow.bias = -0.001;
+
+const ambLight = new THREE.AmbientLight(0xffffff, 0.5);
 scene.add(pointLight, ambLight);
 
 // 6. Helpers
@@ -102,13 +118,18 @@ renderer.setAnimationLoop((currentTime) => {
   // Delta time ensures rotation speed is frame-rate independent
   const elapsedTime = currentTime * 0.0001;
 
-  earth.rotation.y += 0.04;
+  earth.rotation.y += 0.01;
 
   earth.position.x = Math.sin(elapsedTime) * 25;
   earth.position.z = Math.cos(elapsedTime) * 25;
 
+  sun.rotation.y += 0.0005;
+
+  // moon.position.x = Math.sin(elapsedTime * 100) * 2;
+  // moon.position.z = Math.cos(elapsedTime * 100) * 2;
+
   let sunBurnIntensityFactor = Math.sin(elapsedTime * 8);
-  sunMat.emissiveIntensity = 1 + Math.abs(sunBurnIntensityFactor);
+  sunMat.emissiveIntensity = 0.85 + Math.abs(sunBurnIntensityFactor);
   pointLight.intensity += 2 * sunBurnIntensityFactor;
 
   controls.update(); // Required when enableDamping is true
